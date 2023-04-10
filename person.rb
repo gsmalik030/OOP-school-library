a5828b32e25b6bab2d3b241b33c8fee2e7832b93
Ghulam Subbhani <gsmalik030@gmail.com>
2023-04-10 12:51:30 +0200

    Added student class

--- person.rb
+++ person.rb
@@ -1,21 +1,17 @@
 class Person
-    attr_accessor :name, :age
-    attr_reader :id
-  
-    def initialize(age, name = 'Unknown', parent_permission: true)
+  attr_accessor :name, :age
+  attr_reader :id
+
+  def initialize(age, name = 'Unknown', parent_permission: true)
       @id = Random.rand(1..100)
-      @name = name,
-      @age = age,
+      @name = name
+      @age = age
       @parent_permission = parent_permission
-    end
-  
-    def can_use_services?
-      if isof_age? || @parent_permission
-        true
-      else
-        false
-      end
-    end
+  end
+
+  def can_use_services?
+    isof_age? || @parent_permission
+  end
   
     private
   
@@ -23,4 +19,3 @@ class Person
       @age >= 18
     end
   end
-
