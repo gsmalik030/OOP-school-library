06a7a811abb5892bd03a802de9ba7a1b077dfb13
Ghulam Subbhani <gsmalik030@gmail.com>
2023-04-10 13:00:38 +0200

    Added teacher class and fixed linters errors

--- person.rb
+++ person.rb
@@ -3,19 +3,19 @@ class Person
   attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
-      @id = Random.rand(1..100)
-      @name = name
-      @age = age
-      @parent_permission = parent_permission
+    @id = Random.rand(1..100)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
   end
 
   def can_use_services?
     isof_age? || @parent_permission
   end
-  
-    private
-  
-    def isof_age?
-      @age >= 18
-    end
+
+  private
+
+  def isof_age?
+    @age >= 18
   end
+end
