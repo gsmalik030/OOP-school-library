6ef9d5ea5306c3683f175de6104929e15eae5868
Ghulam Subbhani <gsmalik030@gmail.com>
2023-04-11 12:08:21 +0200

    Fixed rubocop errors

--- base_decorator.rb
+++ base_decorator.rb
@@ -2,10 +2,11 @@ require_relative 'nameable'
 
 class BaseDecorator < Nameable
   def initialize(nameable)
+    super()
     @nameable = nameable
-end
+  end
 
-def correct_name
-  @nameable.correct_name
-end
+  def correct_name
+    @nameable.correct_name
+  end
 end
--- person.rb
+++ person.rb
@@ -5,6 +5,7 @@ class Person < Nameable
   attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..100)
     @name = name
     @age = age
