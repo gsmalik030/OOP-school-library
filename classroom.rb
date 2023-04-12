c885bbeba01c9b6537b639ae2272701be33cda7c
Ghulam Subbhani <gsmalik030@gmail.com>
2023-04-12 10:21:54 +0200

    Added classroom class

---
+++ classroom.rb
@@ -0,0 +1,10 @@
+class Classroom
+    attr_accessor :lable :students
+def initialize(lable)
+    @lable = lable
+    @students = []
+end
+def add_students = (student)
+    @student.push(student)
+    student.update_classroom(self)
+end
--- student.rb
+++ student.rb
@@ -1,5 +1,6 @@
 require './person'
 class Student < Person
+  attr_accessor :classroom
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
     @classroom = classroom
@@ -7,5 +8,12 @@ class Student < Person
 
   def play_hooky
     '"¯\(ツ)/¯"'
+  end
+  def update_classroom(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless 
+    classroom.students.include?(self)
+      
+    end
   end
 end
